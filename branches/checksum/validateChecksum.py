ca40d4f69a7e4b6a17cca3aed77662909233925e
hc5023295@gmail.com <hc5023295@gmail.com@49a189f7-b3dc-7159-b598-6f3ec0faa391>
2012-05-21 13:16:52 +0000

    修改校验脚本；版本升级至5.1.1；去除旧的无用图片；新增1个通用规则；修改“搜狗+搜狐”分类为“搜狗&搜狐&搜狐畅游”；新增“搜狐畅游”广告过滤。

--- branches/checksum/validateChecksum.py
+++ branches/checksum/validateChecksum.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python
 #coding=utf-8
 #version=1.0
- 
+
+#############################################################################
+# This is a reference script to validate the checksum in downloadable       #
+# subscription. This performs the same validation as Adblock Plus when it   #
+# downloads the subscription.                                               #
+#                                                                           #
+# To validate a subscription file, run the script like this:                #
+#                                                                           #
+#   python validateChecksum.py adblock-list.txt                             #
+#                                                                           #
+# Note: your subscription file should be saved in UTF-8 encoding, otherwise #
+# the validation result might be incorrect.                                 #
+#                                                                           #
+# This script is wrote by Gythialy for ChinaList Project                    #
+#############################################################################
+
 import base64
 import codecs
 import hashlib
@@ -9,21 +24,21 @@ import os
 import re
 import sys
 import string
- 
+
 def read(path):
     f = codecs.open(path, 'rt', encoding='utf-8')
     data = f.read()
     f.close()
     return data
- 
+
 def calculatchecksum(content):
     content = re.sub(r"\r\n", "\n", content)
     content = re.sub(r"\n+", "\n", content)
     m = hashlib.md5(content.encode('utf-8'))
     validate = base64.b64encode(m.digest())
- 
+
     return re.sub(r"=+$", "", validate)
- 
+
 def validate(path):
     data = read(path)
     prog = re.compile(r"\s*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n", re.I)
@@ -41,7 +56,7 @@ def validate(path):
         print filename + " 's checksum is valid."
     else:
         print 'Wrong checksum: found {0}, expected {1}'.format(checksum, validate)
- 
+
 if __name__ == '__main__':
     filename = ''
     if len(sys.argv) != 2:

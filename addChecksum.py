1e5f9c6f376e913f8be04a8131ad662b50cdebae
hc5023295@gmail.com <hc5023295@gmail.com@49a189f7-b3dc-7159-b598-6f3ec0faa391>
2012-05-21 13:16:52 +0000

    修改校验脚本；版本升级至5.1.1；去除旧的无用图片；新增1个通用规则；修改“搜狗+搜狐”分类为“搜狗&搜狐&搜狐畅游”；新增“搜狐畅游”广告过滤。

--- addChecksum.py
+++ addChecksum.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python
 #coding=utf-8
 #version=1.0
- 
+
+#############################################################################
+# This is a reference script to add checksums to downloadable               #
+# subscriptions. The checksum will be validated by Adblock Plus on download #
+# and checksum mismatches (broken downloads) will be rejected.              #
+#                                                                           #
+# To add a checksum to a subscription file, run the script like this:       #
+#                                                                           #
+#   python addChecksum.py adblock.txt  					                    #
+#                                                                           #
+# Note: your subscription file should be saved in UTF-8 encoding, otherwise #
+# the generated checksum might be incorrect.                                #
+#                                                                           #
+# This script is wrote by Gythialy for ChinaList Project                    #
+#############################################################################
+
 import os
 import codecs
 import hashlib
@@ -9,39 +24,39 @@ import base64
 import re
 import string
 import sys
- 
+
 def read(path):
     f = codecs.open(path, 'rt', encoding='utf-8')
     data = f.read()
     f.close()
     return data
- 
+
 def save(content, path):
     f = codecs.open(path, 'w', encoding='utf-8')
     f.write(content)
     f.close()
- 
+
 def calculatchecksum(content):
     content = re.sub(r"\r\n", "\n", content)
     content = re.sub(r"\n+", "\n", content)
     m = hashlib.md5(content.encode('utf-8'))
     validate = base64.b64encode(m.digest())
- 
+
     return re.sub(r"=+$", "", validate)
- 
+
 def insert(original, new):
     pos = original.index(']') + 2
     return original[:pos] + new + original[pos:]
- 
+
 def removechecksum(content):
     prog = re.compile(r"\s*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n", re.I)
     match = prog.search(content)
     if match:
         temp = match.group().strip()
         content = string.replace(content, temp, "")
- 
+
     return content
- 
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print 'Pls input file name.'
@@ -54,7 +69,7 @@ if __name__ == '__main__':
     print 'Began to insert checksum'
     content = read(filename)
     content = removechecksum(content)
- 
+
     checksum = '!  Checksum: {0}'.format(calculatchecksum(content))
     content = insert(content, checksum)
     save(content, path)

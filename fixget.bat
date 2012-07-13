a55a58cc06c3e4c8cbd084ac799700ec4fc015d7
hc5023295 <hc5023295@49a189f7-b3dc-7159-b598-6f3ec0faa391>
2012-07-13 06:44:12 +0000

    增加xcffl.tk修复。

--- fixget.bat
+++ fixget.bat
@@ -7,6 +7,7 @@ echo %TempFile_Name%
 if exist %TempFile_Name% (
 echo 正在尝试修复过滤规则获取问题……
 @echo 203.208.47.1 adfiltering-rules.googlecode.com>>c:\windows\system32\drivers\etc\hosts
+@echo 31.170.163.99 xcffl.tk>>c:\windows\system32\drivers\etc\hosts
 echo 已完成，请尝试重新获取规则。
 ) else (
 echo 没有以管理员身份运行当前批处理，无法修复过滤规则获取问题。

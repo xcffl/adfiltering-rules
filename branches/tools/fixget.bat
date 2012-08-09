b752901285f2e1aba75b3a8a002982f115ae4512
hc5023295@gmail.com <hc5023295@gmail.com@49a189f7-b3dc-7159-b598-6f3ec0faa391>
2012-08-09 03:02:13 +0000

    [转换脚本/部署工具]修正opera版规则头；修正fixget.bat失效问题。

--- branches/tools/fixget.bat
+++ branches/tools/fixget.bat
@@ -6,8 +6,8 @@ echo %TempFile_Name%
 
 if exist %TempFile_Name% (
 echo 正在尝试修复过滤规则获取问题……
-@echo 173.194.79.82 adfiltering-rules.googlecode.com>>c:\windows\system32\drivers\etc\hosts
-@echo 31.170.163.99 xcffl.tk>>c:\windows\system32\drivers\etc\hosts
+173.194.79.82 adfiltering-rules.googlecode.com>>c:\windows\system32\drivers\etc\hosts
+31.170.163.99 xcffl.tk>>c:\windows\system32\drivers\etc\hosts
 echo 已完成，请尝试重新获取规则。
 ) else (
 echo 没有以管理员身份运行当前批处理，无法修复过滤规则获取问题。

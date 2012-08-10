51999a3ac484ffcc0ec7bebe92c92949640c7343
hc5023295@gmail.com <hc5023295@gmail.com@49a189f7-b3dc-7159-b598-6f3ec0faa391>
2012-08-10 05:25:08 +0000

    修正AB Pro、Avast版换行问题。

--- branches/transform/转换AB Pro.py
+++ branches/transform/转换AB Pro.py
@@ -285,13 +285,7 @@ def writeRule(filePath, lines):
   top = u'[General]'
   
   #规则头
-  head = u'''name=广告过滤软件通用强效广告过滤规则 for AB Pro
-updateUrl=http://adfiltering-rules.googlecode.com/svn/trunk/lastest/rules_for_AB_PRO.txt
-updateTime=1
-[Whitelist]
-[Block Address]
-Copyright 2011 xcffl, Apache License 2.0
-'''
+  head = u'''name=广告过滤软件通用强效广告过滤规则 for AB Pro\r\nupdateUrl=http://adfiltering-rules.googlecode.com/svn/trunk/lastest/rules_for_AB_PRO.txt\r\nupdateTime=1\r\n[Whitelist]\r\n[Block Address]\r\nCopyright 2011 xcffl, Apache License 2.0\r\n'''
   #规则尾
   bottom = '[Block Object]'
   #输出到文件  
